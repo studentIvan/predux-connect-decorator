0f1b9fae451a607b7b4365ab8a52614689555390
Ivan Maslov <cryptox3@gmail.com>
2017-08-21 20:23:58 +0400

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { connect } from 'react-redux';
+import { connect } from 'preact-redux';
 import { bindActionCreators } from 'redux';
 
 const connectDecorator = (matchPropsToState, actions) => {
